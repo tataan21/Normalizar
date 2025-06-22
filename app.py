aa5c45beb50e00d15a228d17fa0443360fb72b93
tataan21 <si.shepherd21@outlook.com>
2025-06-22 11:11:08 -0400

    cambio a app

--- app.py
+++ app.py
@@ -151,4 +151,6 @@ def descargar():
     return send_file(ultimo_excel, download_name="datos_normalizados.xlsx", as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
